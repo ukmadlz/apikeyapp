df78e2a6cc30030f5e9744d521b11b08fc999684
Mike Elsmore <mike@elsmore.me>
2016-01-27 15:28:37 +0000

    First run at API key generator API for mobile apps

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # apikeyapp
 Generates Cloudant API keys and passes the URL and keys to the offline app
+
+
+This is to accompany a talk at DroidCon Bucharest 2016
+
+## Trains, Planes & Automobiles, your app still works.
+
+Everyone one uses apps in some way on smart phones, be it Android, iOS and even webapps. And a lot of apps try and look the same across the platforms, but a lot of the time they use very different underlying technology.
+
+This talk is about creating cross-platform apps that use the same base technologies to make a development teams life a lot easier.
---
+++ index.js
@@ -0,0 +1,109 @@
+// Licensed under the Apache License, Version 2.0 (the 'License'); you may not
+// use this file except in compliance with the License. You may obtain a copy of
+// the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an 'AS IS' BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+
+'use strict';
+
+// Required Modules
+require('dotenv').load({ silent: true });
+var Hapi = require('hapi');
+var Path = require('path');
+var Cloudant = require('cloudant');
+
+// Config services
+// VCAP Services
+var vcapServices = JSON.parse(process.env.VCAP_SERVICES);
+
+// Setup Cloudant
+var cloudantCreds = vcapServices.cloudantNoSQLDB[0].credentials;
+var cloudant = Cloudant({
+  account: cloudantCreds.username,
+  password: cloudantCreds.password,
+});
+var database = process.env.DATABASE;
+
+// Instantiate the server
+var server = new Hapi.Server({
+  debug: {
+    request: ['error', 'good'],
+  },
+  connections: {
+    routes: {
+      files: {
+        relativeTo: Path.join(__dirname, 'public'),
+      },
+    },
+  },
+});
+
+// Set the Hapi Connection
+server.connection({
+  host: process.env.VCAP_APP_HOST || 'localhost',
+  port: process.env.VCAP_APP_PORT || process.env.PORT || 3000,
+  routes: {
+    cors: true,
+  },
+});
+
+// Route to return the url to access a Cloudant DB
+server.route({
+    method: ['GET', 'POST'], // Must handle both GET and POST
+    path: '/',          // The callback endpoint registered with the provider
+    handler: (request, reply) => {
+      cloudant.generate_api_key(function(er, api) {
+        if (er) {
+          throw er; // You probably want wiser behavior than this.
+        };
+
+        var db = cloudant.db.use(database);
+
+        // Or you can read the security settings from a database.
+        db.get_security(function(er, result) {
+          if (er) {
+            throw er;
+          }
+
+          var security = result.cloudant;
+          security[api.key] = ['_reader', '_writer', '_replicator'];
+
+          db.set_security(security, function(er, result) {
+            if (er) {
+              throw er;
+            };
+
+            var url = 'https://' +
+            api.key +
+            ':' +
+            api.password +
+            '@' +
+            cloudantCreds.host +
+            '/' +
+            database;
+            reply({
+              url: url,
+            }).code(200);
+          });
+        });
+      });
+    },
+  });
+
+// Hapi Log
+server.log(['error', 'database', 'read']);
+
+// Start Hapi
+server.start(function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Server started at: ' + server.info.uri);
+  }
+});
