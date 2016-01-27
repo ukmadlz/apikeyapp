c15519d1db0b4733870207f7c93b6a8322b39314
Mike Elsmore <mike@elsmore.me>
2016-01-27 20:51:19 +0000

    Add some bad code that now handles 1 DB per User #1

--- index.js
+++ index.js
@@ -60,11 +60,44 @@ server.connection({
 });
 
 // Error return
-var errorFunc = function(er, callback) {
+var errorFunc = function(error, callback) {
   callback({
-    error: er.message,
-  }).code(er.statusCode);
+    error: error.message,
+  }).code(error.statusCode);
 };
+
+// Process security
+var processSecurity = function(database, api, reply) {
+  var db = cloudant.db.use(database);
+
+  // Or you can read the security settings from a database.
+  db.get_security(function(er, result) {
+    if (er) {
+      errorFunc(er, reply);
+    } else {
+      var security = (result.cloudant) ? result.cloudant : {};
+      security[api.key] = ['_reader', '_writer', '_replicator'];
+
+      db.set_security(security, function(er, result) {
+        if (er) {
+          errorFunc(er, reply);
+        } else {
+          var url = 'https://' +
+          api.key +
+          ':' +
+          api.password +
+          '@' +
+          cloudantCreds.host +
+          '/' +
+          database;
+          reply({
+            url: url,
+          }).code(200);
+        }
+      });
+    }
+  });
+}
 
 // Route to return the url to access a Cloudant DB
 server.route({
@@ -76,35 +109,25 @@ server.route({
           throw er; // You probably want wiser behavior than this.
         };
 
-        var db = cloudant.db.use(database);
-
-        // Or you can read the security settings from a database.
-        db.get_security(function(er, result) {
-          if (er) {
-            errorFunc(er, reply);
-          } else {
-            var security = result.cloudant;
-            security[api.key] = ['_reader', '_writer', '_replicator'];
-
-            db.set_security(security, function(er, result) {
-              if (er) {
-                errorFunc(er, reply);
-              } else {
-                var url = 'https://' +
-                api.key +
-                ':' +
-                api.password +
-                '@' +
-                cloudantCreds.host +
-                '/' +
-                database;
-                reply({
-                  url: url,
-                }).code(200);
-              }
-            });
-          }
-        });
+        if (process.env.DBPERUSER) {
+          database = (request.params.user) ? request.params.user :
+            ((request.query.user) ? request.query.user : api.key);
+          cloudant.db.get(database, function(err, body){
+            if (err) {
+              cloudant.db.create(database, function(err, body) {
+                if (err) {
+                  errorFunc(err, reply);
+                } else {
+                  processSecurity(database, api, reply);
+                }
+              });
+            } else {
+              processSecurity(database, api, reply);
+            }
+          });
+        } else {
+          processSecurity(database, api, reply);
+        }
       });
     },
   });
